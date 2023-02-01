147b6a5202eea16c86c1589131e24d74e325342a
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2023-02-01 09:17:28 +0300

    utils file added

--- tika/video.py
+++ tika/video.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from scipy import ndimage
 import time
+from utils import masking,bounding_box
 import matplotlib.pyplot as plt
 
 # hue values must be discrete
@@ -14,45 +15,6 @@ lower_red = np.array([5,100,110])
 upper_red = np.array([30,210,200])
 
 
-def masking(img, lower_hsv, upper_hsv):
-
-    # creating mask
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
-
-    bitw = cv2.bitwise_and(mask, mask, mask=mask)
-
-    # applying opening operation
-    kernel = np.ones((2, 2), np.uint8)
-    opening = cv2.morphologyEx(bitw, cv2.MORPH_OPEN, kernel)
-
-    # removing parasites
-    mask_f = ndimage.median_filter(opening, size=2)
-
-    return mask_f
-
-def bounding_box(mask):
-    # try:
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    params = []
-    if len(contours) > 0:
-        sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        for c in sorted_contours[:3]:
-            obj_area = cv2.contourArea(c)
-            if obj_area > 60:
-                x,y,w,h = cv2.boundingRect(c)
-                params.append([(x,y),(w+x,h+y)])
-                
-            else:
-                print("no object found bigger than treshold")
-                return None
-        return params
-    
-    else:
-        print("no contour found")
-        return None
-
-
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -62,7 +24,7 @@ prev_image_time = 0
 new_image_time = 0
 a = 0
 
-cap = cv2.VideoCapture('/Users/emirysaglam/Documents/GitHub/IP_general/video.mp4')
+cap = cv2.VideoCapture('C:/Users/ertug/Desktop/video.mp4')
 
 
 while True:
