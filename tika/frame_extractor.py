1c69c2664d4ed7ffc2fdd3fa24229aa54f4a1842
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2023-03-26 16:21:39 +0300

    added new data and video

--- tika/frame_extractor.py
+++ tika/frame_extractor.py
@@ -1,26 +1,33 @@
 import cv2
 
 
-cap = cv2.VideoCapture('C:/Users/ertug/Desktop/video.mp4')
-path = "C:/Users/ertug/Desktop/data"
-c=0
+cap = cv2.VideoCapture('/Users/emirysaglam/Documents/GitHub/IP_general/tika/data/data_collect.mov')
+extract_path = "/Users/emirysaglam/Documents/GitHub/IP_general/tika/data/data_frames/"
+count = 0
+skip = 0
 
 while True:
     ret, image = cap.read()
 
     if not ret:
         break
-
-    cv2.imshow("Image", image)
-    #cv2.imwrite(path + f"{c+1}.png",image)
-    c+=1
-
-    print(path + f"{c+1}.png" + " saved")
-    k = cv2.waitKey(10)
     
+    
+
+    if skip % 9 == 0:
+        cv2.imshow("Image", image[:, 180:900])
+        cv2.imwrite(extract_path + f"tika_{count+1}.jpg",image[:, 180:900])
+        count+=1
+
+        print(extract_path + f"{count+1}.png" + " saved")
+        print(count)
+
+    k = cv2.waitKey(8)
+        
+    skip+=1
     """    
     if k == ord('s'):  
-        cv2.imwrite(path + f"{c+1}.png",image)
+        cv2.imwrite(path + f"{count+1}.png",image)
         c+=1
     """
     if k == ord('p'):  
