2c54ff5e9c9887af896fed9c4a585c0d1dc54397
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2023-04-07 15:37:35 +0300

    class update 1

--- tika/video_main.py
+++ tika/video_main.py
@@ -39,7 +39,7 @@ a = 0
 ################
 
 # fonskiyonun icine 0 atarak kamerayı aktiflestirebilirsin yoksa istedigin videonun yolunu yaz
-cap = cv2.VideoCapture('/Users/emirysaglam/Desktop/video3.mp4')
+cap = cv2.VideoCapture('/Users/emirysaglam/Desktop/tika-video/video3.mp4')
 
 #intersect fonksiyonu icin son 3 maskeyi tutuyo
 red_mask_cache=[]
---
+++ tikaClass.py
@@ -0,0 +1,6 @@
+class tika:
+    def __init__(self,image,lowerHsv,upperHsv):
+        self.image = image
+        self.lowerHsv = lowerHsv
+        self.upperhsv = upperHsv
+        
