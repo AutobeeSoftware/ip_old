8d6a520b4309d5fca97f05755a973007e444222d
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2023-04-09 17:46:16 +0300

    utils update
    added gstreamerpipeline for video capture

--- tika/utils.py
+++ tika/utils.py
@@ -155,5 +155,34 @@ def closest(params):
     else:
         return None
 
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
 
+    """
+    >>cv2.VideoCapture(gstreamer_pipeline()) 
+    """
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 #cumulate fonksiyonu ekle
--- tikaClass.py
+++
@@ -1,6 +0,0 @@
-class tika:
-    def __init__(self,image,lowerHsv,upperHsv):
-        self.image = image
-        self.lowerHsv = lowerHsv
-        self.upperhsv = upperHsv
-        
